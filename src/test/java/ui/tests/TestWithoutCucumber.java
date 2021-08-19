f2f293df38e5d41cc56519e8a7fd3fb302f115ef
Hansjoerg Keser <hansjoerg.keser@bit.admin.ch>
2021-08-19 23:07:10 +0200

    test without cucu

---
+++ src/test/java/ui/tests/TestWithoutCucumber.java
@@ -0,0 +1,19 @@
+package ui.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestWithoutCucumber {
+
+    @Test
+    void testSomething() {
+        // java code...
+        // open browser
+        // search for something
+        // find something
+
+        assertTrue(true, "Something went wrong");
+    }
+
+}
